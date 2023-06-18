ec593074b30b007ef1151f467754e1b98425ae09
Doniyorbek <ac29526@akfauniversity.org>
2023-06-18 17:38:29 +0500

    Initial Commit

---
+++ SwiftUIApiCalls/SwiftUIApiCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApiCallsApp.swift
+//  SwiftUIApiCalls
+//
+//  Created by Doniyorbek Ibrokhimov  on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApiCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
